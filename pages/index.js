21912a3deadcf9fe08acf7872d4029217e6a1291
Matheus Beilfuss <93745995+matheusbeilfuss@users.noreply.github.com>
2025-01-10 13:23:18 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Space: The final frontier. These are the voyages of the starship Enterprise. Its 5-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before.</h1>
+}
+
+export default Home;
