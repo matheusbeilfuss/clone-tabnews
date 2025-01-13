03a70844b48187c727616106414f422caf7b9164
Matheus Beilfuss <93745995+matheusbeilfuss@users.noreply.github.com>
2025-01-13 13:44:38 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Space: The final frontier. These are the voyages of the starship Enterprise. Its 5-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before.</h1>
+    return <h1>Manu, estou aqui e vou continuar aqui, contigo ❤️</h1>
 }
 
 export default Home;
